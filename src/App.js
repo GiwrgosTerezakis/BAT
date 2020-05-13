0de1974f67e37b405fa3fdb102e875bb375b7567
Giwrgos Terezakis <39535101+GiwrgosTerezakis@users.noreply.github.com>
2020-05-13 16:15:48 +0300

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Material UI</h1>
+      <h2>Introduction</h2>
+    </div>
+  );
+}
+
+export default App;
